bc6640af5f064da403e2616b9edf60567f8612b4
github username <github email address>
2019-11-08 15:57:32 -0500

    basic routing done

--- src/container/CarouselContainer.js
+++ src/container/CarouselContainer.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import CarouselSlider from "react-carousel-slider"
 import './CarouselContainer.css'
 import {Carousel} from 'react-bootstrap';
 import SingleCarousel from '../component/SingleCarousel'
 
     let data= [1,2,3]
     const renderCarousel = () => data.map(d => <Carousel.Item><SingleCarousel /></Carousel.Item>)
-
+    
     const CarouselContainer = () => {
     return (
         <Carousel>
---
+++ src/container/FourOhFour.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react'
+
+const FourOhFour = () => {
+    return(
+        <>
+        <h1>Sorry, we're fresh out!</h1>
+        <p>We couldn't find a result for that.</p>
+        </>
+    )
+}
+
+export default FourOhFour
