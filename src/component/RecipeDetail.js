e809a515bd6c5c15e6a831e688a27d7853a27cb0
github username <github email address>
2019-11-11 17:19:55 -0500

    added recipe info

--- src/component/RecipeDetail.js
+++ src/component/RecipeDetail.js
@@ -6,14 +6,30 @@ const RecipeDetail = (props) => {
         const step = props.recipe.steps.find(step => step.number === num)
            return step? step.text : ""
     }
+
+    const getAttributes = () => {
+        const arr = []
+        if(props.recipe.veg) arr.push("V")
+        if(props.recipe.vegan) arr.push("V+")
+        if(props.recipe.dairy_free) arr.push("DF")
+        if(props.recipe.gluten_free) arr.push("GF")
+        if(props.recipe.keto) arr.push("K")
+        if(props.recipe.healthy) arr.push("H")
+        return arr
+    }
+
     return(
         props.recipe ? 
             <div className="single-recipe-detail-container">
                 <h1>{props.recipe.name}</h1>
                 <div className="step">
+                    <p className="recipe-attributes">
+                    <span>{"Serves: "+props.recipe.serves + " | Time to Cook: " + props.recipe.time_in_minute + " minutes"}</span>
+                    {getAttributes().length? <span >{getAttributes().join(" | ")}</span> : null}
+                    </p>
                     <h3>Step {props.stepNum}</h3>
                     <img src={props.recipe.image_url} alt="stepOne" />
-                    <p>{getStepText(props.stepNum)}</p>
+                    <p className="step-text">{getStepText(props.stepNum)}</p>
                 </div>
             </div>
         :
