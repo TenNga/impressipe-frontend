dbb17e0184e97b112cc21fc8f3c966ea9a833433
karma <kevinkarma55@gmail.com>
2019-11-10 20:07:42 -0500

    step list layout done

--- src/component/StepList.js
+++ src/component/StepList.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import './css/StepList.css';
 
 const StepList = () => {
     return(
-        <ul>
-            <li>Step One</li>
-            <li>Step Two</li>
-            <li>Step Three</li>
-            <li>Step Four</li>
-        </ul>
+        <div className="step-list">
+            <ul >
+                <li className="current-step">Step One</li>
+                <li>Step Two</li>
+                <li>Step Three</li>
+                <li>Step Four</li>
+                <li>Step Five</li>
+                <li>Step Six</li>
+                <li>Step Seve</li>
+                <li>Step Eight</li>
+                <li>Step Nine</li>
+                <li>Step Ten</li>
+            </ul>
+        </div>
     )
 }
 export default StepList;
