a826059ec7f97cee59f60aea4313020103aef8ea
karma <kevinkarma55@gmail.com>
2019-11-11 13:50:02 -0500

    adding functionality to show page

--- src/container/RecipeShowContainer.js
+++ src/container/RecipeShowContainer.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react';
 import IngAndEqContainer from './IngAndEqContainer'
 import RecipeDetail from '../component/RecipeDetail'
 import StepList from '../component/StepList'
 import './css/RecipeShowContainer.css'
 
 
-const RecipeShowContainer = (props) => {
-    return(
-        <div className="recipe-detail-container">
-            <IngAndEqContainer />
-            <RecipeDetail recipe={props.recipe}/>
-            <StepList />
-        </div>
-    )
+class RecipeShowContainer extends Component{
+    state = {
+        selectedRecipe: this.props.recipe? this.props.recipe : ""
+    }
+
+    render(){
+        return(
+            <div className="recipe-detail-container">
+                <IngAndEqContainer />
+                <RecipeDetail recipe={this.state.selectedRecipe}/>
+                <StepList steps={this.state.selectedRecipe.steps}/>
+            </div>
+        )
+    }
 }
 export default RecipeShowContainer;
