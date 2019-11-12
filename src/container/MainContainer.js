88f67e4e88a6c2bbcabeb71be9e1d8ac778e4882
github username <github email address>
2019-11-12 14:50:56 -0500

    stable build

--- src/container/MainContainer.js
+++ src/container/MainContainer.js
@@ -10,18 +10,32 @@ import FourOhFour from './FourOhFour'
 
 const MainContainer = (props) => {
 
-    const veg = {categoryName: "Vegetarian", recipes: props.recipes.filter(recipe => recipe.veg === true)}
-    const vegan = {categoryName: "Vegan", recipes: props.recipes.filter(recipe => recipe.vegan === true) }
-    const diary_free = {categoryName: "Dairy Free", recipes: props.recipes.filter(recipe => recipe.dairy_free === true) }
-    const gluten_free = {categoryName: "Gluten Free", recipes: props.recipes.filter(recipe => recipe.gluten_free === true) }
-    const keto = {categoryName: "Keto", recipes: props.recipes.filter(recipe => recipe.keto === true) }
+    function shuffle(array) {
+        let currentIndex = array.length, temporaryValue, randomIndex;
+        while (0 !== currentIndex) {
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex -= 1;
+          temporaryValue = array[currentIndex];
+          array[currentIndex] = array[randomIndex];
+          array[randomIndex] = temporaryValue;
+        }
+      
+        return array;
+      }
+
+    const nonVeg = {categoryName: "Non-Vegetarian", recipes: shuffle(props.recipes.filter(recipe => recipe.veg === false))} 
+    const veg = {categoryName: "Vegetarian", recipes: shuffle(props.recipes.filter(recipe => recipe.veg === true))}
+    const vegan = {categoryName: "Vegan", recipes: shuffle(props.recipes.filter(recipe => recipe.vegan === true)) }
+    const diary_free = {categoryName: "Dairy Free", recipes: shuffle(props.recipes.filter(recipe => recipe.dairy_free === true)) }
+    const gluten_free = {categoryName: "Gluten Free", recipes: shuffle(props.recipes.filter(recipe => recipe.gluten_free === true))}
+    const keto = {categoryName: "Keto", recipes: shuffle(props.recipes.filter(recipe => recipe.keto === true))}
      
     const twentyMinRecipe = {header: "Ready In 20 Minutes", recipes: props.recipes.filter(recipe => parseInt(recipe.time_in_minute) <= 20)};
     const partyTime = {header: "Cooking for a Group?", recipes: props.recipes.filter(recipe => parseInt(recipe.serves) >= 12)};
     const ingFiveOnly = {header: "Just 5 Ingredients", recipes: props.recipes.filter(recipe => parseInt(recipe.ingredients.length) === 5)};
 
     const carouselRecipes = [twentyMinRecipe, partyTime, ingFiveOnly]
-    const categories = [veg,vegan,diary_free,gluten_free,keto]
+    const categories = [nonVeg, veg,vegan,diary_free,gluten_free,keto]
 
     const recipeShowObj = (routerProps) => {
         if(props.recipes.length){
